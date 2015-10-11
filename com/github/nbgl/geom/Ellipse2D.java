c38ed85c7cc46bb871b123bfee7c484d7d797134
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:30:39 +0800

    Create Ellipse2D.java

---
+++ com/github/nbgl/geom/Ellipse2D.java
@@ -0,0 +1,12 @@
+/*
+* Ellipse2D.java                                                                                        *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*This class creates an 2D ellipse drawn at JGameArena.                                                  *
+*/
+package com.github.nbgl.geom;
+
+public abstract class Ellipse2D extends java.awt.geom.Ellipse2D{
+}
