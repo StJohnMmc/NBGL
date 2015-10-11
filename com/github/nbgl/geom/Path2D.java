fbb1ff6e614c6e28885ac0ab2713ca43b71ab01b
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:37:23 +0800

    Create Path2D.java

---
+++ com/github/nbgl/geom/Path2D.java
@@ -0,0 +1,12 @@
+/*
+* Path2D.java                                                                                           *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*This class creates an 2D patn at JGameArena.                                                           *
+*/
+package com.github.nbgl.geom;
+
+public abstract class Path2D extends java.awt.geom.Path2D{
+}
