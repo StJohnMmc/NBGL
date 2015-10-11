4060105314824f7877f042a6df336f6f2190cbad
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:42:12 +0800

    Create RectangularShape.java

---
+++ com/github/nbgl/geom/RectangularShape.java
@@ -0,0 +1,12 @@
+/*
+* RectangularShape.java                                                                                 *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*This class creates an rectangular shape at JGameArena.                                                 *
+*/
+package com.github.nbgl.geom;
+
+public abstract class RectangularShape extends java.awt.geom.RectangularShape{
+}
