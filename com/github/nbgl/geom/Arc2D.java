c3f4c4b303f3f67ef823b189a6ca9a2a0c7b973e
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:23:35 +0800

    Create Arc2D.java

---
+++ com/github/nbgl/geom/Arc2D.java
@@ -0,0 +1,12 @@
+/* 
+* Arc2D.java                                                                                            *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*This class creates an 2D arc drawn at JGameArena.                                                      *
+*/
+package com.github.nbgl.geom;
+
+public abstract class Arc2D extends java.awt.geom.Arc2D{
+}
