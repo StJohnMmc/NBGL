b723d7166c9280f358a40135e90ac005a9e14ff0
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:38:34 +0800

    Create Point2D.java

---
+++ com/github/nbgl/geom/Point2D.java
@@ -0,0 +1,12 @@
+/*
+* Point2D.java                                                                                          *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*This class creates an 2D point at JGameArena.                                                          *
+*/
+package com.github.nbgl.geom;
+
+public abstract class Point2D extends java.awt.geom.Point2D{
+}
