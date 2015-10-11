b7a7707a5ead6608240f93aa0ad945a329b1b77c
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:46:52 +0800

    Create MouseEvent.java

---
+++ com/github/nbgl/gui/event/MouseEvent.java
@@ -0,0 +1,11 @@
+/*
+* MouseEvent.java                                                                                       *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*/
+package com.github.nbgl.geom;
+
+public abstract class MouseEvent extends java.awt.event.MouseEvent{
+}
