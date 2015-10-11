fdb63a56a8e362e2cf5bedb4123b8ce5ffbeac04
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:43:09 +0800

    Create RoundRectangle2D.java

---
+++ com/github/nbgl/geom/RoundRectangle2D.java
@@ -0,0 +1,12 @@
+/*
+* RoundRectangle2D.java                                                                                 *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*This class creates an 2D round rectangle at JGameArena.                                                *
+*/
+package com.github.nbgl.geom;
+
+public abstract class RoundRectangle2D extends java.awt.geom.RoundRectangle2D{
+}
