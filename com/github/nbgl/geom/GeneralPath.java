5cf322aa57042fe47d944f92d9b879dac611f3ee
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:34:39 +0800

    Create GeneralPath.java

---
+++ com/github/nbgl/geom/GeneralPath.java
@@ -0,0 +1,12 @@
+/*
+* GeneralPath.java                                                                                      *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*This class creates an 2D arc drawn at JGameArena.                                                      *
+*/
+package com.github.nbgl.geom;
+
+public abstract class GeneralPath extends java.awt.geom.GeneralPath{
+}
