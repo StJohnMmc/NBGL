c18e6bcac2e1a7d923b40fa0809d12e1e2de03c9
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:22:43 +0800

    Create Area2D.java

---
+++ com/github/nbgl/geom/Area2D.java
@@ -0,0 +1,12 @@
+/*
+* Area2D.java                                                                                           *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*This class creates an 2D area     at JGameArena.                                                       *
+*/
+package com.github.nbgl.geom;
+
+public abstract class Area2D extends java.awt.geom.Area{
+}
