bf8a6618d582587aaad558b521690057f2de3996
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:27:59 +0800

    Create CubicCurve2D.java

---
+++ com/github/nbgl/geom/CubicCurve2D.java
@@ -0,0 +1,12 @@
+/*
+* CubicCurve2D.java                                                                                     *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*This class creates an 2D cubic curve drawn at JGameArena.                                              *
+*/
+package com.github.nbgl.geom;
+
+public abstract class CubicCurve2D extends java.awt.geom.CubicCurve2D{
+}
