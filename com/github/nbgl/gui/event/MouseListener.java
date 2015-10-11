dad88ba90387c7cdfe90f0bad563f853dd6cc983
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:46:05 +0800

    Create MouseListener.java

---
+++ com/github/nbgl/gui/event/MouseListener.java
@@ -0,0 +1,11 @@
+/*
+* MouseListener.java                                                                                    *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*/
+package com.github.nbgl.geom;
+
+public abstract class MouseListener extends java.awt.event.MouseListener{
+}
