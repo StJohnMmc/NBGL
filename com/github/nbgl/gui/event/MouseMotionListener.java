e1aebe430f1c6e50cd8c0532e91caea79b649a59
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:52:30 +0800

    Create MouseMotionListener.java

---
+++ com/github/nbgl/gui/event/MouseMotionListener.java
@@ -0,0 +1,11 @@
+/*
+* MouseMotionListener.java                                                                              *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*/
+package com.github.nbgl.geom.event;
+
+public abstract class MouseMotionListener extends java.awt.event.MouseMotionListener{
+}
