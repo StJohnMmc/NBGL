ffb767242cde0b7efdcffd3e699ebbb6c28b3280
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:36:10 +0800

    Create Line2D.java

---
+++ com/github/nbgl/geom/Line2D.java
@@ -0,0 +1,12 @@
+/*
+* Line2D.java                                                                                           *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*This class creates an 2D line drawn at JGameArena.                                                     *
+*/
+package com.github.nbgl.geom;
+
+public abstract class Line2D extends java.awt.geom.Line2D{
+}
