9b31b2de8485fe6f0c928fe4d48f12664304f10a
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:39:45 +0800

    Create QuadCurve2D.java

---
+++ com/github/nbgl/geom/QuadCurve2D.java
@@ -0,0 +1,12 @@
+/*
+* QuadCurve2D.java                                                                                      *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*This class creates an 2D quad curve drawn at JGameArena.                                               *
+*/
+package com.github.nbgl.geom;
+
+public abstract class QuadCurve2D extends java.awt.geom.QuadCurve2D{
+}
