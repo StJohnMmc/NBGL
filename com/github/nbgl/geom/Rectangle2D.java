2ef252d03a9489352565cef135b3e8339384b23b
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:40:53 +0800

    Create Rectangle2D.java

---
+++ com/github/nbgl/geom/Rectangle2D.java
@@ -0,0 +1,12 @@
+/*
+* Rectangle2D.java                                                                                      *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*This class creates an 2D rectangle drawn at JGameArena.                                                *
+*/
+package com.github.nbgl.geom;
+
+public abstract class Rectangle2D extends java.awt.geom.Rectangle2D{
+}
