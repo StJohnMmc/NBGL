e9ac3fd0f6746a9a1db58f923869c510f1b4875c
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:29:20 +0800

    Create Dimension2D.java

---
+++ com/github/nbgl/geom/Dimension2D.java
@@ -0,0 +1,12 @@
+/*
+* Dimension2D.java                                                                                      *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*This class creates an 2D dimension at JGameArena.                                                      *
+*/
+package com.github.nbgl.geom;
+
+public abstract class Dimension2D extends java.awt.geom.Dimension2D{
+}
