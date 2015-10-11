f30372282602711545eca278731580c98b68d935
StJohnMmc <kingmakers_specialist777@yahoo.com>
2015-10-11 15:25:09 +0800

    Create Transform.java

---
+++ com/github/nbgl/geom/Transform.java
@@ -0,0 +1,12 @@
+/*
+* Transform.java                                                                                        *
+*                                                                                                       *
+*(c)Copyright - 2015                                                                                    *
+*Written by Baldvicio,Jonathan Eldy I.                                                                  *
+*                                                                                                       *
+*This class transforms shapes that is drawn at JGameArena                                               *
+*/
+package com.github.nbgl.geom;
+
+public abstract class Transform extends java.awt.geom.AfflineTransform{
+}
